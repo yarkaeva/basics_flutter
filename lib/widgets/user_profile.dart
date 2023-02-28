b0dbf5f214e3330aa6fa95e38e827555e442feab
Darina Y <darina.yarkaeva@gmail.com>
2023-02-28 15:20:06 +0500

    Finished lesson 21, row, expanded,icon

--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -1,11 +1,52 @@
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
-  const UserProfile({super.key});
+  UserProfile({super.key});
+  final List<MenuRowData> firstMenuRow = [
+    MenuRowData(
+      Icons.favorite,
+      'Избранное',
+    ),
+    MenuRowData(
+      Icons.call,
+      'Недавние звонки',
+    ),
+    MenuRowData(
+      Icons.computer_outlined,
+      'Устройства',
+    ),
+    MenuRowData(
+      Icons.folder_outlined,
+      'Папки с чатами',
+    ),
+  ];
+  final List<MenuRowData> secondMenuRow = [
+    MenuRowData(
+      Icons.notification_add,
+      'Уведомления и звуки',
+    ),
+    MenuRowData(
+      Icons.privacy_tip,
+      'Конфиденциальность',
+    ),
+    MenuRowData(
+      Icons.date_range,
+      'Данные и память',
+    ),
+    MenuRowData(
+      Icons.brush,
+      'Оформление',
+    ),
+    MenuRowData(
+      Icons.language,
+      'Язык',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade300,
       appBar: AppBar(
         title: const Text('Настройки'),
       ),
@@ -14,17 +55,86 @@ class UserProfile extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            SizedBox(height: 30),
-            _AvatarWidget(),
-            SizedBox(height: 30),
-            _UserNameWidject(),
-            SizedBox(height: 10),
-            _UserPhoneWidget(),
-            SizedBox(height: 10),
-            _UserNickNameWidget(),
+          children: [
+            const _UserInfo(),
+            const SizedBox(height: 30),
+            _MenuWidget(menuRow: firstMenuRow),
+            const SizedBox(height: 30),
+            _MenuWidget(menuRow: secondMenuRow),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  MenuRowData(this.icon, this.text);
+
+  final IconData icon;
+  final String text;
+}
+
+class _MenuWidget extends StatelessWidget {
+  const _MenuWidget({super.key, required this.menuRow});
+  final List<MenuRowData> menuRow;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: menuRow
+            .map((data) => _MenuWidgetRow(
+                  data: data,
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  const _MenuWidgetRow({super.key, required this.data});
+  final MenuRowData data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(data.icon),
+          const SizedBox(width: 15),
+          Expanded(child: Text(data.text)),
+          const Icon(Icons.chevron_right_sharp),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserInfo extends StatelessWidget {
+  const _UserInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: const [
+          SizedBox(height: 30),
+          _AvatarWidget(),
+          SizedBox(height: 30),
+          _UserNameWidject(),
+          SizedBox(height: 10),
+          _UserPhoneWidget(),
+          SizedBox(height: 10),
+          _UserNickNameWidget(),
+        ],
       ),
     );
   }
@@ -41,7 +151,7 @@ class _UserNickNameWidget extends StatelessWidget {
       '@JonFir',
       style: TextStyle(
         color: Colors.grey,
-        fontSize: 18,
+        fontSize: 17,
       ),
     );
   }
