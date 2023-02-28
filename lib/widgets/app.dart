58e61253f910e63c0d60d2b2834f1a56a1b79eff
Darina Y <darina.yarkaeva@gmail.com>
2023-02-28 12:11:21 +0500

    Saved forggoten file app.dart

--- lib/widgets/app.dart
+++ lib/widgets/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_lesson_1/widgets/user_profile.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const UserProfile();
   }
 }
